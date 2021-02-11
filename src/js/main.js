d25e2bdebe982ac00aca15191c6a4dd851f6fa67
Galina-lucky <gala.nitch@gmail.com>
2021-02-11 16:05:25 +0200

    Optimized main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,39 +1,36 @@
 ;(function(){
 
   $(document).ready(function() {
-    let main = document.querySelector('.js-main');
+    let mainHero = document.querySelector('.js-mainHero');
     let enemy1 = document.querySelector('.js-hero-enemy-1');
     let enemy2 = document.querySelector('.js-hero-enemy-2');
     let enemy3 = document.querySelector('.js-hero-enemy-3');
-    let pointsMain;
+    let pointsMainHero;
     let pointsEnemy1;
     let pointsEnemy2;
     let pointsEnemy3;
 
-    pointsMain = getRndInteger(2, 10);
-    pointsEnemy1 = getRndInteger(1, pointsMain);
-    pointsEnemy2 = getRndInteger(pointsEnemy1 + 1, pointsMain + pointsEnemy1);
-    pointsEnemy3 = getRndInteger(pointsEnemy2 + 1, pointsMain + pointsEnemy1 + pointsEnemy2);
+    pointsMainHero = getRndInteger(2, 10);
+    pointsEnemy1 = getRndInteger(1, pointsMainHero);
+    pointsEnemy2 = getRndInteger(pointsEnemy1 + 1, pointsMainHero + pointsEnemy1);
+    pointsEnemy3 = getRndInteger(pointsEnemy2 + 1, pointsMainHero + pointsEnemy1 + pointsEnemy2);
 
-    main.innerHTML = pointsMain;
+    mainHero.innerHTML = pointsMainHero;
     enemy1.innerHTML = pointsEnemy1;
     enemy2.innerHTML = pointsEnemy2;
     enemy3.innerHTML = pointsEnemy3;
 
-    main.ondragstart = function() {
+    mainHero.ondragstart = function() {
       return false;
     };
 
     // потенциальная цель переноса, над которой мы пролетаем прямо сейчас
-    let currentenemy = null;
-    let mainCoordinates = {};
-    let enemy1Coordinates = {};
-    let enemy2Coordinates = {};
-    let enemy3Coordinates = {};
+    let currentEnemy = null;
+    let mainHeroCoordinates = {};
 
-    enemy1Coordinates = playerCoordinates(enemy1);
-    enemy2Coordinates = playerCoordinates(enemy2);
-    enemy3Coordinates = playerCoordinates(enemy3);
+    let enemy1Coordinates = playerCoordinates(enemy1);
+    let enemy2Coordinates = playerCoordinates(enemy2);
+    let enemy3Coordinates = playerCoordinates(enemy3);
 
     let enemyList = {
       1: {
@@ -53,66 +50,55 @@
       },  
     }
     
-    main.onmousedown = function(event) {
+    mainHero.onmousedown = function(event) {
 
-      let shiftX = event.clientX - main.getBoundingClientRect().left; //коорд курсора в герое
-      let shiftY = event.clientY - main.getBoundingClientRect().top;  //коорд курсора в герое
-
-      main.style.zIndex = 1000;
-
-      moveAt(event.pageX, event.pageY);
+      let shiftX = event.clientX - mainHero.getBoundingClientRect().left; //коорд курсора в герое
+      let shiftY = event.clientY - mainHero.getBoundingClientRect().top;  //коорд курсора в герое
 
       //координаты верхнего левого угла героя (xLeft,yTop)
       function moveAt(pageX, pageY) {
-        main.style.left = pageX - shiftX + 'px';
-        main.style.top = pageY - shiftY + 'px';
+        mainHero.style.left = pageX - shiftX + 'px';
+        mainHero.style.top = pageY - shiftY + 'px';
       }
 
       function onMouseMove(event) {
         moveAt(event.pageX, event.pageY);
 
-        mainCoordinates = playerCoordinates(main);
-        //console.log(mainCoordinates);
+        mainHeroCoordinates = playerCoordinates(mainHero);
+        //console.log(mainHeroCoordinates);
 
         for (let key in enemyList) {
           let enemyCoord = enemyList[key]["coordinates"];
           let enemy = document.getElementById(enemyList[key]["id"]);
-         // console.log(enemyCoord);
-
-          if ((mainCoordinates["xL"] <= enemyCoord["xR"]) &&
-              (mainCoordinates["xR"] >= enemyCoord["xL"]) &&
-              (mainCoordinates["yT"] <= enemyCoord["yB"]) &&
-              (mainCoordinates["yB"] >= enemyCoord["yT"])) {
-
+         
+          if ((mainHeroCoordinates["xL"] <= enemyCoord["xR"]) &&
+             (mainHeroCoordinates["xR"] >= enemyCoord["xL"]) &&
+             (mainHeroCoordinates["yT"] <= enemyCoord["yB"]) &&
+             (mainHeroCoordinates["yB"] >= enemyCoord["yT"])) {
             enemy.style.background = 'yellow';
           }
+        }
 
-
-      }
-
-              
-
-
-        main.hidden = true;
+        mainHero.hidden = true;
         let elemBelow = document.elementFromPoint(event.clientX, event.clientY); // элемент, где координаты курсора на поле (не на герое)
-        main.hidden = false;
+        mainHero.hidden = false;
 
         if (!elemBelow) return; //если героя вынесли за пределы экрана
 
         let enemy = elemBelow.closest('.hero--enemy'); //
 
-        currentenemy = enemy;
-        if (currentenemy) {
+        currentEnemy = enemy;
+        if (currentEnemy) {
           // логика обработки процесса, когда мы "влетаем" в элемент droppable
-          enterDroppable(currentenemy);
+          enterDroppable(currentEnemy);
         }
       }
 
       document.addEventListener('mousemove', onMouseMove);
 
-      main.onmouseup = function() {
+      mainHero.onmouseup = function() {
         document.removeEventListener('mousemove', onMouseMove);
-        main.onmouseup = null;
+        mainHero.onmouseup = null;
       };
     }    
 
@@ -121,37 +107,24 @@
       console.log(elem);
 
       playerCoordinates(elem); //координаты противника
-      playerCoordinates (main); //координаты героя
+      playerCoordinates (mainHero); //координаты героя
 
       pointsMain = pointsMain + parseInt(elem.innerText);
       elem.hidden = true;
       console.log(pointsMain);
-      main.innerHTML = pointsMain;
-
-      
+      mainHero.innerHTML = pointsMain;      
     }
   });
 
+  // ------ Functions ------
+  
   function playerCoordinates (el) {
-    let xLeft = el.getBoundingClientRect().left;
-    let xRight = el.getBoundingClientRect().right;
-    let yTop = el.getBoundingClientRect().top;
-    let yBottom = el.getBoundingClientRect().bottom;
-
-    /*let coordinates = {
-      "LT": {x: xLeft, y: yTop},
-      "RT": {x: xRight, y: yTop},
-      "LB": {x: xLeft, y: yBottom},
-      "RB": {x: xRight, y: yBottom},
-    };*/
-
     let coordinates = {
-      "xL" : xLeft,
-      "xR" : xRight,
-      "yT" : yTop,
-      "yB": yBottom,
+      "xL" : el.getBoundingClientRect().left,
+      "xR" : el.getBoundingClientRect().right,
+      "yT" : el.getBoundingClientRect().top,
+      "yB": el.getBoundingClientRect().bottom,
     };
-
     return coordinates;
   }
 
